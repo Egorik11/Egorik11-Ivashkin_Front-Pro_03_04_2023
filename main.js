8b17bd3baae8b3a28fde469643f96b02bbf6ea0e
Yehor Ivashkin <ivashin.egor98@gmail.com>
2023-06-02 22:20:28 +0300

    hw_26

--- main.js
+++ main.js
@@ -1,119 +1,49 @@
-const data = [
-  {
-    src: 'img/Apple iPhone 14 Pro.jpg',
-    categories: 'Phone',
-    info: {
-      memory: '128Gb',
-      display: '6.1", 2556x1179 (19.5:9), 460 ppi, 120 Гц',
-    },
-  },
-  {
-    src: 'img/Xiaomi Redmi Note 11.jpg',
-    categories: 'Phone',
-    info: {
-      memory: '6/128Gb',
-      display: '6.43", 2400x1080 (20:9), 409 ppi, 90 Гц',
-    },
-  },
-  {
-    src: 'img/Google Pixel 6a.jpg',
-    categories: 'Phone',
-    info: {
-      memory: '6/128Gb',
-      display: '6.1", 2400x1080 (20:9), 428 ppi',
-    },
-  },
-  {
-    src: 'img/Samsung Galaxy M33.jpg',
-    categories: 'Phone',
-    info: {
-      memory: '6/128Gb',
-      display: '6.6", 2408x1080 (20:9), 400 ppi, 120 Гц',
-    },
-  },
-  {
-    src: 'img/MTB Crossbike Leader 29.jpg',
-    categories: 'Bicycle',
-    info: {
-      manufacturer: 'CrossBike',
-      wheel: '29", двойной алюминиевый обод',
-    },
-  },
-  {
-    src: 'img/Like.Bike Loon 26.jpg',
-    categories: 'Bicycle',
-    info: {
-      manufacturer: 'Like.Bike',
-      wheel: '26", двойной алюминиевый обод',
-    },
-  },
-  {
-    src: 'img/Engwe T14.jpg',
-    categories: 'Bicycle',
-    info: {
-      manufacturer: 'Engwe',
-      wheel: '14", алюминиевый обод',
-    },
-  },
-  {
-    src: 'img/Halmar Harissa B.jpg',
-    categories: 'Table',
-    info: {
-      size: '420х420х410 мм',
-    },
-  },
-  {
-    src: 'img/Signal Lucca.jpg',
-    categories: 'Table',
-    info: {
-      size: '920х600х520 мм',
-    },
-  },
-  {
-    src: 'img/Столик-трансформер.jpg',
-    categories: 'Table',
-    info: {
-      size: '1200х600х410 мм',
-    },
-  },
-];
+const url = 'https://jsonplaceholder.typicode.com/posts/';
+const submit = document.querySelector('#submit');
 
-const container = document.querySelector('.container');
-const viewCategories = document.querySelector('.viewCategories');
-const select = document.querySelector('#select');
-const showInfo = document.querySelector('.showInfo');
-const blockInfo = document.querySelector('.info');
+function getId() {
+  const inputId = document.querySelector('#inputId');
+  let targetValue;
+  const reqExp = /^([1-9]|[1-9][0-9]|100)$/;
 
-function createMarkupInfo(el) {
-  const objInfo = el.info;
-  const mainDiv = document.createElement('div');
-  for (const key in objInfo) {
-    const p = document.createElement('p');
-    p.textContent = `${key}: ${objInfo[key]};  `;
-    mainDiv.appendChild(p);
-  }
-  return mainDiv;
-}
-
-function showInfoImg(img, el, parent, infoContainer) {
-  img.addEventListener('click', () => {
-    infoContainer.textContent = createMarkupInfo(el);
-  });
-  parent.appendChild(img);
-  blockInfo.appendChild(infoContainer);
-}
-
-select.addEventListener('change', (e) => {
-  const target = select.value;
-  viewCategories.innerHTML = '';
-  showInfo.innerHTML = '';
-
-  data.forEach((el) => {
-    if (target === el.categories) {
-      const img = document.createElement('img');
-      img.src = el.src;
-
-      showInfoImg(img, el, viewCategories, showInfo);
+  inputId.addEventListener('keyup', (e) => {
+    targetValue = e.target.value;
+    inputId.classList.remove('valid', 'invalid');
+    if (reqExp.test(targetValue)) {
+      inputId.classList.add('valid');
+    } else {
+      inputId.classList.add('invalid');
     }
   });
+  return () => Number(targetValue);
+}
+
+const getTargetValue = getId();
+
+function makeRequest(url, id) {
+  return fetch(`${url}${id}`).then((res) => {
+    if (!res.ok) {
+      throw new Error(`Error ${res.status}`);
+    }
+    return res.json();
+  });
+}
+
+function createMarkup(post) {
+  const postContainer = document.querySelector('#post');
+  const p = document.createElement('p');
+  p.innerHTML = `id: ${post.id} <br/> title: ${post.title} <br/> body: ${post.body}`;
+  postContainer.appendChild(p);
+}
+
+submit.addEventListener('click', (e) => {
+  const postContainer = document.querySelector('#post');
+  postContainer.innerHTML = '';
+  makeRequest(url, getTargetValue())
+    .then((data) => {
+      createMarkup(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
